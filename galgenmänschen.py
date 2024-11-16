bee3287798d5f55e887c7ac3711ae49e36bdf002
Miro-Strg <miro.mathys@ict-scouts.ch>
2024-11-16 10:57:54 +0100

    Wörterbuch

--- galgenmänschen.py
+++ galgenmänschen.py
@@ -1,14 +1,21 @@
-def Galgenmännchen():
-    geheimes_wort = "baumhaus"
+import random
+
+Tipps = {'Brot': 'Hartes Essen', 'MacBook': 'laptop', 'MacOS': 'Wird von ITlern gehasst', 'Linux': 'Lieben ITler', 'Holzdecke': 'Holz'}
+Wörterbuch = ['Brot', 'MacBook', 'MacOS', 'Linux', 'Holzdecke']
+def Galgenmännchen(geheimes_wort):
     versuche = 6
     geratene_buchstaben = []
 
     while versuche > 0:
-        print('wort:',"".join(buchstabe if buchstabe in geratene_buchstaben
+        print('wort:',"".join(buchstabe if buchstabe.lower() in geratene_buchstaben
                               else'_'for buchstabe in geheimes_wort))
         print('versuche übrig:', versuche)
 
         eingabe = input ('gib einen Buchstaben ein: ').lower()
+
+        if eingabe == 'Info': 
+            print ()
+            continue
 
         if len(eingabe) == 0 or not eingabe.isalpha():
             print('Bitte minedstend einen buchstaben eingeben')
@@ -19,23 +26,27 @@ def Galgenmännchen():
 
         geratene_buchstaben .append(eingabe)
 
-        if eingabe in geheimes_wort:
+        if eingabe in geheimes_wort.lower():
             print('Der Buchstabe '+ eingabe + ' ist drin!')
 
         else:
             versuche -= 1
             print('Falscher Buchstabe. Versuche es erneut')
 
-        if all(buchstabe in geratene_buchstaben for buchstabe in geheimes_wort):
+        if all(buchstabe in geratene_buchstaben for buchstabe in geheimes_wort.lower()):
                 print('Herzlichen Glückwunsch! Sie haben das Wort' +geheimes_wort+'erraten!')
                 break
 
-    print('Keine versuche mehr übrig! Das wort war' + geheimes_wort +'Spiel vorbei.')
+    print('Keine versuche mehr übrig! Das wort war ' + geheimes_wort +' Spiel vorbei.')
 
     nochmal_spielen = input('Möchtest du nochmal spielen? (Ja/nein): ')
     if nochmal_spielen.lower() == 'ja':
-        Galgenmännchen()
+         
+        Wort= random.choice (Wörterbuch)
+        Galgenmännchen(Wort)
+    
     else:
-        print('Vielen Dank für das Spielen von Galgenmännchen!')
+      print('Vielen Dank für das Spielen von Galgenmännchen!')
 
-Galgenmännchen()
+Wort= random.choice (Wörterbuch)
+Galgenmännchen(Wort)
